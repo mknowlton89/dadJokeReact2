0686008c7ae6cae1b55079e407122feb73488991
Michael Knowlton <mknowlton89@gmail.com>
2021-04-24 20:13:20 -0400

    Got the top-nav wired up.

--- dadjokes/src/screens/Navbar/component.js
+++ dadjokes/src/screens/Navbar/component.js
@@ -6,39 +6,30 @@ import {
     Link
 } from "react-router-dom";
 import './styles.css';
-<link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-    crossorigin="anonymous"
-/>
 
 
 export function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
+        <nav>
+            <div className="nav-container">
                 <Link to="/">
-                    <p className="navbar-brand">Dad Joke Generator</p>
+                    <p className="nav-logo">Dad Joke Generator</p>
                 </Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link to="/">
-                                <p className="nav-link active" aria-current="page">Home</p>
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/favorites">
-                                <p className="nav-link">Favorites</p>
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+
+                <ul>
+                    <li>
+                        <Link to="/">
+                            <p>Home</p>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/favorites">
+                            <p>Favorites</p>
+                        </Link>
+                    </li>
+                </ul>
             </div>
+
         </nav>
     )
 }
